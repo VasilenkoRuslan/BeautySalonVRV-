c3dd514cf5a5b81e4d4dc08712f0cdc49c7537ee
Ruslan Vasilenko <vasilenko.ruslan0321@gmail.com>
2021-05-07 21:27:09 +0300

    ADD | credentials_sample.php

---
+++ credentials_sample.php
@@ -0,0 +1,15 @@
+<?php
+$cred = array(
+    "dev" => array(
+        'DB_NAME' => "beauty",
+        'DB_USER' => "root",
+        'DB_PASSWORD' => "1",
+        'DB_HOST' => "localhost"
+    ),
+    "prod" => array(
+        'DB_NAME' => "beauty",
+        'DB_USER' => "beauty",
+        'DB_PASSWORD' => "bD9vP8uD0vtL2p",
+        'DB_HOST' => "localhost"
+    )
+);
